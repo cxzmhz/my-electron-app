ada4484f3b85cef9d722328957dbff4789cff33e
chenxuezhong6 <chenxuezhong6@crland.com.cn>
2025-07-12 18:11:49 +0800

    feat: 新增明暗主题

--- main.js
+++ main.js
@@ -1,14 +1,44 @@
-const {app, BrowserWindow} = require('electron');
+const { app, BrowserWindow, ipcMain, nativeTheme } = require('electron/main')
+const path = require('node:path')
 
-const createWindow = () => {
+function createWindow () {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
-  });
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
 
-  win.loadFile('index.html');
+  win.loadFile('index.html')
+  win.webContents.openDevTools(); // 自动打开网页的 DevTools
 }
 
-app.whenReady().then(()=>{
-  createWindow();
+ipcMain.handle('dark-mode:toggle', () => {
+  if (nativeTheme.shouldUseDarkColors) {
+    nativeTheme.themeSource = 'light'
+  } else {
+    nativeTheme.themeSource = 'dark'
+  }
+  return nativeTheme.shouldUseDarkColors
+})
+
+ipcMain.handle('dark-mode:system', () => {
+  nativeTheme.themeSource = 'system'
+})
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
